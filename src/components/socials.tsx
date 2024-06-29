0b9a323ae6674df9b5c10918c50b48058eb9eb7c
Jyotideep Acharjee <jdp_jan@outlook.com>
2024-06-29 18:46:50 +0530

    github logo

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -1,5 +1,4 @@
-import { FaInstagram, FaSnapchat, FaXTwitter } from "react-icons/fa6";
-import { FiGithub } from "react-icons/fi";
+import { FaInstagram, FaSnapchat, FaXTwitter, FaGithub } from "react-icons/fa6";
 import { SlSocialLinkedin } from "react-icons/sl";
 import { LiaTelegramPlane } from "react-icons/lia";
 import { AiOutlineDiscord } from "react-icons/ai";
@@ -7,7 +6,7 @@ import { CiFacebook } from "react-icons/ci";
 
 export const iconMapping:{[key: string]: JSX.Element} = {
     'LinkedIn': <SlSocialLinkedin size={32} className="fill-white"/>,
-    'Github': <FiGithub size={32} className="fill-white"/>,
+    'Github': <FaGithub size={32} className="fill-white"/>,
     'Instagram': <FaInstagram size={32} className="fill-white"/>,
     'Telegram': <LiaTelegramPlane size={32} className="fill-white"/>,
     'Snapchat': <FaSnapchat size={32} className="fill-white"/>,
